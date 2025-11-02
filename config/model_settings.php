38eeee7d62ea3d4dfee44e086a982e650fdc88de
Marco Kaiser <marco@kaiser.kiwi>
2025-11-02 06:48:10 +0100

    feat: Add caching config entries - Make table name configurable via the environment file
    Refs: #3

--- config/model_settings.php
+++ config/model_settings.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-	'table' => 'model_settings',
+	'table' => env('MODEL_SETTINGS_TABLE', 'model_settings'),
+
+	'caching' => [
+		'enabled' => env('MODEL_SETTINGS_CACHE_ENABLED', false),
+		'ttl' => env('MODEL_SETTINGS_CACHE_TTL', 60 * 60 * 24 * 30), // 30 days in seconds
+		'key_prefix' => env('MODEL_SETTINGS_CACHE_KEY_PREFIX', 'model_settings'),
+	],
 ];
